9174e17136df18cf403453a5e1a1d0436a6af287
rafsan15-10903 <rafsan15-10903@diu.edu.bd>
2025-11-22 20:25:31 +0600

    problem solving commit

--- problem-solving.js
+++ problem-solving.js
@@ -25,3 +25,66 @@ else{
 }
 const year1 = isLeapYear(2021);
 console.log(year1);
+
+// even number gor ber kora
+
+function evenNumbers (number){
+    const evenNumberArray = [];
+    for(const num of number){
+        if(num % 2 === 0){
+            evenNumberArray.push(num);
+        }
+    }
+    
+    let sum = 0;
+    for(num of evenNumberArray){
+        sum = sum +num;
+    }
+    const count = evenNumberArray.length;
+    console.log(sum, count);
+    const average = sum / count;
+    return  average;
+}
+const numbers = [1,2,3,4,5,6,7,8,9];
+const result1 = evenNumbers(numbers);
+console.log('average of the even num is', result1);
+
+// odd number er gor ber kora
+
+ function oddNumber (number){
+    let newArray = [];
+    for(const num of number){
+        if(num % 2 !== 0){
+            newArray.push(num)
+        }
+    }
+    console.log(newArray);
+    let sum = 0;
+    for(num of newArray){
+     sum = sum + num;
+    }
+    console.log(sum)
+    let count = newArray.length;
+    console.log(count);
+    let average = sum / count;
+    console.log(average, 'this is the average')
+    // return average;
+}
+ const finalNum = [1,2,3,4,5,6,7,8,9];
+ const finalOddNumber = oddNumber(finalNum);
+
+ // duplicate item array theke bad dea
+
+ function noDuplicate(array){
+    newArray = [];
+    for(num of array){
+        if(newArray.includes(num) === false){
+            newArray.push(num);
+        }
+    }
+    return newArray;
+ }
+
+ const setOfArray = ['a', 'b', 'c', 'a', 'g', 'h', 'k'];
+ const newArrayFinal = noDuplicate(setOfArray)
+ console.log(newArrayFinal);
